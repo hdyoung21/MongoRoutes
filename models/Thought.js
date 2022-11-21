e1601e6b698b856081ed39044e3a0ae29f76aedd
hdyoung21 <112896050+hdyoung21@users.noreply.github.com>
2022-11-21 18:02:50 -0500

    models

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,21 @@
 const { Schema, Model } = require('mongoose');
+const { moveMessagePortToContext } = require('worker_threads');
 
 
 const thoughtSchema = mew Schema(
-    {}
-)
+    {
+        thoughtText: {
+            type: String,
+            require: true,
+            max_length: 280,
+            min_length: 1,
+        },
+    },
+
+username: [
+    {
+        type: String,
+        required: true
+    }
+],
+
